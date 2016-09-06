baeb3f6b81782d64ddf39da5896bb7d87e74f562
Changfeng Liu <steven24liu@gmail.com>
2016-09-06 10:56:03 -0500

    minor change

--- questions.php
+++ questions.php
@@ -3,9 +3,22 @@
 </b>
 
 <?php
+function openFile() {
+    $currentTime = date("Y-m-d-h-i-sa");
+    $fileName = "UserResults/" . $currentTime . ".txt";
+
+    if (file_exists ( $fileName )) {
+        sleep(1);
+        return openFile();
+    }
+    else{
+        $myfile = fopen($fileName, "a") or die("Unable to open file!");
+        return $myfile;
+    }
+}
+
 date_default_timezone_set("America/Chicago");
-$currentTime = date("Y-m-d-h-i-sa");
-$myfile = fopen("UserResults/" . $currentTime . ".txt", "a") or die("Unable to open file!");
+$myfile = openFile();
 
 //submit time
 fwrite($myfile,"Chicago Time: ");
@@ -16,39 +29,37 @@ fwrite($myfile,"\n");
 fwrite($myfile,"1\n");
 
 //bin 1~5
-fwrite($myfile, $_POST["level1_set"]);
+fwrite($myfile, test_input($_POST["level1_set"]));
 fwrite($myfile,"\n");
 
-fwrite($myfile, $_POST["level2_set"]);
+fwrite($myfile, test_input($_POST["level2_set"]));
 fwrite($myfile,"\n");
 
-fwrite($myfile, $_POST["level3_set"]);
+fwrite($myfile, test_input($_POST["level3_set"]));
 fwrite($myfile,"\n");
 
-fwrite($myfile, $_POST["level4_set"]);
+fwrite($myfile, test_input($_POST["level4_set"]));
 fwrite($myfile,"\n");
 
-fwrite($myfile, $_POST["level5_set"]);
+fwrite($myfile, test_input($_POST["level5_set"]));
 fwrite($myfile,"\n");
 
 //questions
-fwrite($myfile, $_POST["difficulty"]);
+fwrite($myfile, test_input($_POST["difficulty"]));
 fwrite($myfile,"\n");
 
-fwrite($myfile, $_POST["confidence"]);
+fwrite($myfile, test_input($_POST["confidence"]));
 fwrite($myfile,"\n");
 
-fwrite($myfile, $_POST["features"]);
-fwrite($myfile,"\n");
-
+fwrite($myfile, test_input($_POST["features"]));
 fwrite($myfile,"\n");
 
 fclose($myfile);
 
-//function test_input($data) {
-//    $data = trim($data);
-//    $data = stripslashes($data);
-//    $data = htmlspecialchars($data);
-//    return $data;
-//}
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>

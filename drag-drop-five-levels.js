4063cf87f4451d1af5bda7d74445320869ee904d
Changfeng Liu <steven24liu@gmail.com>
2016-09-29 02:59:15 +0800

    debug

--- drag-drop-five-levels.js
+++ drag-drop-five-levels.js
@@ -270,11 +270,12 @@ function createDygraphs() {
     }
 
     $("#DataCollection > div").each(function (index) {
-        //debug
-        var content = readTextFile("data/".concat(dataset_name, "/query", query_index, "/", tsName, ".csv"));
-        console.log("%s -- No.%d TS.",content,index+1);
-
         var tsName = $(this).attr("id");
+
+        //debug
+        var tmp = readTextFile("data/".concat(dataset_name, "/query", query_index, "/", tsName, ".csv"));
+        console.log("%s -- No.%d TS.",tmp,index+1);
+
         tsNameArray[index+1] = tsName;
         tsDygraphs[index+1] = new Dygraph(
             // document.getElementById(tsName),
